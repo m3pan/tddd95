92a131a643ae57f91921134b04803ab87d342ae9
Madeleine Nilsson <59937917+m3pan@users.noreply.github.com>
2023-01-19 00:14:47 +0100

    👹 wrong output
    debug tomorrow! time to sleep 🌃

--- exercises/help.cc
+++ exercises/help.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream> // file stream
 #include <sstream>
+#include <map>
 
 int main() {
 
@@ -10,6 +11,7 @@ int main() {
     std::string line{};
     std::vector <std::vector<std::string>> lines{};
     std::string word{};
+
 
     while (getline(ifs, line, '\r'))
     {
@@ -24,6 +26,46 @@ int main() {
     int nr = std::stoi(lines[0][0]);
     lines.erase(lines.begin(), lines.begin() + 1);
 
+    for (int i = 0; i < nr; i++)
+    {
+        std::vector<std::string> line1 {lines[i]};
+        std::vector<std::string> line2 {lines[i + 1]};
+        std::string output{};
+        for (int j = 0; j < line1.size(); j++)
+        {
+            // adds the placeholders to maps
+            std::map<char, std::string> patterns1;
+            std::map<char, std::string> patterns2;
+
+            if (line1[j][0] == '<' && line2[j][0]!= '<')
+            {
+                if (patterns1.count(line1[j][1]) == false)
+                // if we haven't added the placeholder to the map
+                {
+                    patterns1[line1[j][1]] = line2[j];
+                }
+                if (patterns1[line1[j][1]] == line2[j])
+                {
+                    output += ' ' + line2[j];
+                }
+
+            }
+
+            else if (line2[j][0] == '<' && line1[j][0]!= '<')
+            {
+                if (patterns2.count(line2[j][1]) == false)
+                    // if we haven't added the placeholder to the map
+                {
+                    patterns2[line2[j][1]] = line1[j];
+                }
+                if (patterns2[line2[j][1]] == line1[j])
+                {
+                    output += ' ' + line1[j];
+                }
+            }
+        }
+        std::cout << output << std::endl;
+    }
 
     return 0;
 }
