d807311ab19b885e1015faf616efb0c694f774b8
Madeleine Nilsson <59937917+m3pan@users.noreply.github.com>
2023-01-30 11:07:43 +0100

    🧹

--- main.cpp
+++ main.cpp
@@ -4,11 +4,3 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
-
-/* stuff
- * #include <streambuf>
- *     // to check what's 'in filestream
-    std::string str((std::istreambuf_iterator<char>(ifs)),
-                    std::istreambuf_iterator<char>());
- *
- * */
