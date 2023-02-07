3856ede6639fe8dfa2a991cbad1e7fdbce3651b8
Madeleine Nilsson <59937917+m3pan@users.noreply.github.com>
2023-02-07 11:29:11 +0100

    🧮 🧮 🧮 ACCEPTED
    mycket tack vare riktigt bra testfall, woop woop!!!

--- exercises/perfectPthPowers.cc
+++ exercises/perfectPthPowers.cc
@@ -3,10 +3,6 @@
 #include <cmath>
 #include <sstream>
 
-// m^k = n
-// primtalsfaktorisera och kolla om man kan skriva talet som multipel av ett och samma tal ?
-// generera en sekvens av tal genom att ändra m och k tills du kommer upp i, eller över n?
-// Sätt b = floor(sqrt(x)) & kör en approach genom att gå nedåt
 
 void determineP(int input);
 
@@ -15,93 +11,46 @@ int main() {
     std::string inputString;
     int integer;
 
-    while(std::getline (std::cin, inputString, '\n')) //!! never stops with terminal input
+    while(std::getline (std::cin, inputString, '\n'))
     {
         std::stringstream ss(inputString);
         ss >> integer;
-        determineP(integer);
+        if (integer)
+        {
+            determineP(integer);
+        } else {
+            break;
+        }
     }
-
     return 0;
 }
-void determineP(int input)
-{
-    if (input < 2)
-    {
-        return;
-    }
-    if (input < 4)
-    {
+void determineP(int input) {
+    if (fabs(input) < 4) {
         std::cout << 1 << "\n";
         return;
-    } /*
-    double squareroot = sqrt(input);
-    if (squareroot == floor(squareroot))
-    {
-        std::cout << 2;
-        return;
-    }*/
-    double k{};
-    for (int m = 2; m <= input/2; ++m) {
-        k = (log(input) / log(m));
-        if (k == floor(k))
-        {
-            std::cout << k << "\n";
+    }
+    double p{};
+    int limit = ceil(sqrt(fabs(input)));
+
+    for (long long int b = 2; b <= limit; b++) {
+        if (input % b != 0) {
+            continue;
+        }
+        p = (log(fabs(input)) / log(b));
+        if (p > 1 && (p - floor(p) < 1e-11)) {
+            if (input == fabs(input))
+            {
+                std::cout << p << "\n";
+            } else {
+                if (int(p) % 2 == 0) {
+                    continue;
+                }
+                else {
+                    std::cout << p << "\n";
+                }
+            }
             return;
         }
     }
     std::cout << 1 << "\n";
-    return;
-
-
-    // Find prime factors
-    /*
-    std::priority_queue<int> primeFactors{};
-    int divideInput{input};
-    while (input % 2 == 0)
-    {
-        primeFactors.push(-2);
-        input = input/2;
-    }
-
-    // n must be odd at this point. So we can skip
-    // one element (Note i = i +2)
-    for (int i = 3; i <= sqrt(divideInput); i = i + 2)
-    {
-        // While i divides n, print i and divide n
-        while (divideInput % i == 0)
-        {
-            primeFactors.push(-divideInput);
-            divideInput = divideInput/i;
-        }
-    }
-
-    // This condition is to handle the case when n
-    // is a prime number greater than 2
-    if (divideInput > 2)
-        primeFactors.push(-divideInput);
-
-    int primeFactor{};
-    int pMax{};
-    // Begin by determining the largest possible p från log av minsta primtalsgrej
-    while (!primeFactors.empty())
-    {
-        primeFactor = primeFactors.top();
-        primeFactors.pop();
-        pMax = log2(input) / log2(primeFactor);
-        for (int b = 2; p != 0; p--)
-        {
-
-        }
-
-
-    }
-
-    // int pMax = std::floor(log2(input));
-    std::cout << pMax << "\n";
-    for (int p = pMax; p != 0; p--)
-    {
-    }*/
-
-
 }
