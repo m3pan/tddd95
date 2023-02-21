6d0b853a7c499aa65a6e27ac8adedf56f04255c8
Madeleine Nilsson <59937917+m3pan@users.noreply.github.com>
2023-02-21 17:09:28 +0100

    😀 no!!! implement sparse algorithm instead
    tack magnus för att du sparade mig någon timme

--- labs/shortestPath1.cc
+++ labs/shortestPath1.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <utility>  //  std::pair
 
 /* Implement Dijkstras algorithm to find the shortest path from one node to all other nodes in a graph with
  * non-negative edge weights.
@@ -9,6 +10,7 @@
  *
  * Suggested API:
  * distance/parent[] shortest_path(graph G, node start)
+ * Är vår graf sparse?
  * */
 
 /* 4 3 4 0
@@ -24,11 +26,15 @@
 1
 0 0 0 0 */
 
-std::vector<int> shortestPath(std::vector<std::vector<int>>edges, int startingNode);
+// todo: implement the sparse algorithm instead!
 
+int inf = 10000000;
+
+std::vector<int> shortestPath(std::vector<std::vector<std::pair<int, int>>> adjacent, int startingNode,
+                              std::vector<std::vector<int>> queries, std::vector<int> & distance,
+                              std::vector<int> & predecessors);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
 
     int nrNodes{};          // n
     int nrEdges{};          // m
@@ -44,20 +50,24 @@ int main() {
         ss1 >> nrEdges;
         ss1 >> nrQueries;
         ss1 >> startingNode;
-        std::vector<std::vector<int>> edges{};
+        std::vector<std::vector<std::pair<int, int>>> adjacent{};
         std::vector<int> result{};
         for (int m = 0; m < nrEdges; m++)
         {
             std::vector<int> edgeVec{};
             std::getline(std::cin, readLine, '\n');
             std::stringstream ss2{readLine};
-            for (int i = 0; i < 3; i++)
-            {
-                int input{};
-                ss2 >> input;
-                edgeVec.push_back(input);
-            }
-            edges.push_back(edgeVec);
+
+            // Read input line consisting of [u v w] = [startNode endNode weight]
+            int u{};
+            int v{};
+            int w{};
+            ss2 >> u;
+            ss2 >> v;
+            ss2 >> w;
+            std::pair<int, int> edge{v, w};
+
+            adjacent[edgeVec[0]].push_back(edge);
         }
 
         std::vector<std::vector<int>> queries{};
@@ -73,9 +83,62 @@ int main() {
                 queriesVec.push_back(input);
             }
             //queries.push_back(queriesVec);
-            result  = shortestPath(edges, startingNode);
+            std::vector<int> distance{};
+            std::vector<int> predecessors{};
+            result  = shortestPath(adjacent, startingNode, queries, distance, predecessors);
         }
-
     }
     return 0;
 }
+
+std::vector<int> shortestPath(std::vector<std::vector<std::pair<int, int>>> adjacent, int startingNode,
+                              std::vector<std::vector<int>> queries, std::vector<int> & distance,
+                              std::vector<int> & predecessors)
+{
+    /*
+     * std::vector<int> distance : for each vertex v, we store the current shortest path from s to v
+     *
+     * std::vector<int> predecessors : for each vertex v != s, p[v] is the penultimate vertex in the shortest path
+     * from s to v
+     * */
+
+    // Number of nodes:
+    int nrNodes = adjacent.size();
+
+    distance.assign(nrNodes, inf);
+    predecessors.assign(nrNodes, -1);
+
+    // A boolean array u[] which stores for each vertex v whether it's marked
+    std::vector<bool> markedVertices(nrNodes, false);
+
+    distance[startingNode] = 0;
+
+    for (int i = 0; i < nrNodes; i++)
+    {
+        /* At each iteration the vertex v is selected which has the smallest distance d[v] among all the unmarked values
+         *
+         *
+         * */
+
+        // The selected vertex which has the smallest distance to the vertex i
+        int v = -1;
+
+        for (int j = 0; j < nrNodes; j++)
+        {
+            if (!markedVertices[j] && (v == -1 || distance[j] < distance[v]))
+            {
+                v = j;
+            }
+        }
+
+        if (distance[v] == inf) break;
+
+        //
+        markedVertices[v] = true;
+
+        for (auto edge : adjacent[v])
+        {
+        }
+    }
+}
+
