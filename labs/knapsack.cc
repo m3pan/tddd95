c1cda562dd67aee2b34b5cb1a5c14cc00665628b
Madeleine Nilsson <59937917+m3pan@users.noreply.github.com>
2023-06-28 00:22:50 +0200

    😪 time limit exceeded

--- labs/knapsack.cc
+++ labs/knapsack.cc
@@ -13,66 +13,29 @@
  * Recommended time complexity: O(n·capacity).
  * */
 
-//!? we’re assuming that there are no massless, valuable items
-// todo: change so that the table stores a std::vector of the picked items
-
 
 struct Item {
     int value{};
     int weight{};
 };
 
+
 int getValue(const std::vector <Item>& items, const std::vector<int>& pickedItems) {
     int sum{};
     for (int i : pickedItems) {
+        if (i == -1) {
+            sum += 0;
+        }
         sum += items[i].value;
     }
     return sum;
 }
 
-std::vector<int> knapsack(int currCapacity, const std::vector <Item>& items, unsigned long currItem, std::vector <std::vector <std::vector<int>>>& table);
 
-int main() {
-    int capacity{};
-    unsigned long nrItems{};
-    std::string line{};
-    std::cout << "hello" << std::endl;
-
-    while (std::cin >> capacity >> nrItems){
-        std::vector<Item> items{};
-        int value{};
-        int weight{};
-        for (int i{}; i < nrItems; ++i) {
-            std::cin >> value >> weight;
-            Item temp{value, weight};
-            items.push_back(temp);
-        }
-        std::cout << "no" << std::endl;
-
-        /* For each test case, output a line containing the number of items chosen, followed by a line containing
-         * the indices of the chosen items (the first item has index, the second index, and so on). The indices can
-         * be given in any order.*/
-
-        // The entry point to solve the 0-1 Knapsack problem using dynamic programming. The function initializes
-        // std::vector <int> chosenItems = knapsack(capacity, items);
-        /* Create a 2-dimensional vector where an entry in row i, column j represents the maximum value that can be
-         * * obtained with items 1, 2, 3 … i, in a knapsack that can hold j weight units. */
-        nrItems = items.size();
-
-        std::vector<std::vector<std::vector<int>>> table(nrItems, std::vector<std::vector<int>>(capacity + 1, {-1}));
-        std::vector<int> pickedItems{};
-        pickedItems = knapsack(capacity, items, nrItems - 1, table);
-        //std::cout << maxValue << std::endl;
-        for (int it : pickedItems){
-            std::cout << it << std::endl;
-        }
-    }
-    return 0;
-}
-
-std::vector<int> knapsack(int currCapacity, const std::vector <Item>& items, unsigned long currItem, std::vector <std::vector <std::vector<int>>>& table) {
+std::vector<int> knapsack(int currCapacity, const std::vector <Item>& items, unsigned long currItem, std::vector<std::vector<std::vector<int>>>& table) {
     /*
      * This recursive function determines the items to pick in order to obtain the maximum value for the knapsack.
+     * I need to store the items selected in each of those sub-solutions as well, and then aggregate this at the end.
      * */
 
     // BASE CONDITIONS
@@ -82,7 +45,7 @@ std::vector<int> knapsack(int currCapacity, const std::vector <Item>& items, uns
     }
 
     // Base condition: If the value has already been computed and stored in the table we return it directly
-    if (table[currItem][currCapacity] != std::vector<int>({-1})) {
+    if (table[currItem][currCapacity] != std::vector<int>{-1}) {
         return table[currItem][currCapacity];
     }
 
@@ -95,14 +58,59 @@ std::vector<int> knapsack(int currCapacity, const std::vector <Item>& items, uns
 
     // Recursive case: Store the maximum value by either including or excluding the current item
     // Including the item:
-    int inclVal = items[currItem].value + getValue(items, knapsack(currCapacity - items[currItem].weight, items, currItem -1, table));
-    int exclVal = getValue(items, knapsack(currCapacity, items, currItem - 1, table));
-    // int inclVal = items[currItem].value + knapsack(currCapacity - items[currItem].weight, items, currItem -1, table, pickedItems);
-    //int exclVal = knapsack(currCapacity, items, currItem - 1, table, pickedItems);
+    auto inclItems = knapsack(currCapacity - items[currItem].weight, items, currItem - 1, table);
+    int inclVal = items[currItem].value + getValue(items, inclItems);
+
+    auto exclItems = knapsack(currCapacity, items, currItem - 1, table);
+    int exclVal = getValue(items, exclItems);
 
     if (inclVal > exclVal) {
-        // add element to vector in table
-        table[currItem][currCapacity].push_back((int)currItem);
+        table[currItem][currCapacity] = inclItems;
+        table[currItem][currCapacity].push_back(currItem);
+    } else {
+        table[currItem][currCapacity] = exclItems;
     }
+
     return table[currItem][currCapacity];
+}
+
+
+int main() {
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int capacity{};
+    int nrItems{};
+
+    while (std::cin >> capacity >> nrItems){
+        std::vector<Item> items{};
+        int value{};
+        int weight{};
+        for (int i{}; i < nrItems; ++i) {
+            std::cin >> value >> weight;
+            Item temp{value, weight};
+            items.push_back(temp);
+        }
+
+        /* For each test case, output a line containing the number of items chosen, followed by a line containing
+         * the indices of the chosen items (the first item has index, the second index, and so on). The indices can
+         * be given in any order.*/
+
+        /* Create a 2-dimensional vector where an entry in row i, column j represents the maximum value that can be
+         * obtained with items 1, 2, 3 … i, in a knapsack that can hold j weight units. */
+        std::vector<std::vector<std::vector<int>>> table(nrItems, std::vector<std::vector<int>>(capacity + 1, std::vector<int>(1, -1)));
+
+        std::vector <int> chosenItems = knapsack(capacity, items, nrItems - 1, table);
+
+        // Terminal output
+        std::cout << chosenItems.size() << "\n";
+        for (int it : chosenItems) {
+            if (it == -1) {continue;}
+            std::cout << it << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
 }
