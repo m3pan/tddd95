ea1a93028e05ee123214b8a2861a6b8c067f3181
Madeleine Nilsson <59937917+m3pan@users.noreply.github.com>
2023-06-27 15:36:27 +0200

    🤡 skiten funkar inte

--- labs/knapsack.cc
+++ labs/knapsack.cc
@@ -14,19 +14,29 @@
  * */
 
 //!? we’re assuming that there are no massless, valuable items
+// todo: change so that the table stores a std::vector of the picked items
+
 
 struct Item {
     int value{};
     int weight{};
 };
 
-int knapsackRecursive(int currCapacity, const std::vector <Item>& items, unsigned long currItem, std::vector <std::vector <int>>& table);
+int getValue(const std::vector <Item>& items, const std::vector<int>& pickedItems) {
+    int sum{};
+    for (int i : pickedItems) {
+        sum += items[i].value;
+    }
+    return sum;
+}
 
-int knapsack(int capacity, const std::vector <Item>& items);
+std::vector<int> knapsack(int currCapacity, const std::vector <Item>& items, unsigned long currItem, std::vector <std::vector <std::vector<int>>>& table);
 
 int main() {
     int capacity{};
-    int nrItems{};
+    unsigned long nrItems{};
+    std::string line{};
+    std::cout << "hello" << std::endl;
 
     while (std::cin >> capacity >> nrItems){
         std::vector<Item> items{};
@@ -37,43 +47,30 @@ int main() {
             Item temp{value, weight};
             items.push_back(temp);
         }
+        std::cout << "no" << std::endl;
 
         /* For each test case, output a line containing the number of items chosen, followed by a line containing
          * the indices of the chosen items (the first item has index, the second index, and so on). The indices can
          * be given in any order.*/
 
+        // The entry point to solve the 0-1 Knapsack problem using dynamic programming. The function initializes
         // std::vector <int> chosenItems = knapsack(capacity, items);
-        int maxValue = knapsack(capacity, items);
-        std::cout << maxValue << std::endl;
+        /* Create a 2-dimensional vector where an entry in row i, column j represents the maximum value that can be
+         * * obtained with items 1, 2, 3 … i, in a knapsack that can hold j weight units. */
+        nrItems = items.size();
+
+        std::vector<std::vector<std::vector<int>>> table(nrItems, std::vector<std::vector<int>>(capacity + 1, {-1}));
+        std::vector<int> pickedItems{};
+        pickedItems = knapsack(capacity, items, nrItems - 1, table);
+        //std::cout << maxValue << std::endl;
+        for (int it : pickedItems){
+            std::cout << it << std::endl;
+        }
     }
     return 0;
 }
 
-int knapsack(int capacity, const std::vector <Item>& items)
-{
-    /*
-     * The entry point to solve the 0-1 Knapsack problem using dynamic programming. The function initializes
-     *
-     * Parameters:
-     *
-     * Return value:
-     * Returns the value inside the table, i.e. the maximum value the knapsack can hold
-     * */
-    unsigned long nrItems = items.size();
-    // todo: move this code snippet to main function
-    // todo: change so that the table stores a std::vector of the picked items
-
-    /* Create a 2-dimensional vector where an entry in row i, column j represents the maximum value that can be
-     * obtained with items 1, 2, 3 … i, in a knapsack that can hold j weight units. */
-    std::vector <std::vector <int>> table( nrItems, std::vector<int> (capacity + 1, -1));
-
-    /* Now, we want to begin populating our table. As with all dynamic programming solutions, at each step, we
-     * will make use of our solutions to previous sub-problems. */
-
-    return knapsackRecursive(capacity, items, nrItems - 1, table);
-}
-
-int knapsackRecursive(int currCapacity, const std::vector <Item>& items, unsigned long currItem, std::vector <std::vector <int>>& table) {
+std::vector<int> knapsack(int currCapacity, const std::vector <Item>& items, unsigned long currItem, std::vector <std::vector <std::vector<int>>>& table) {
     /*
      * This recursive function determines the items to pick in order to obtain the maximum value for the knapsack.
      * */
@@ -81,32 +78,31 @@ int knapsackRecursive(int currCapacity, const std::vector <Item>& items, unsigne
     // BASE CONDITIONS
     // Base condition: No more items or weight to consider
     if (currCapacity == 0 || currItem == 0) {
-        return 0;
+        return {};
     }
 
     // Base condition: If the value has already been computed and stored in the table we return it directly
-    if (table[currItem][currCapacity] != -1) {
+    if (table[currItem][currCapacity] != std::vector<int>({-1})) {
         return table[currItem][currCapacity];
     }
 
     // RECURSIVE CONDITIONS
     // Recursive case: Checks if the weight of the nth item is more than the knapsack's currCapacity and should be ignored
     if (items[currItem].weight > currCapacity) {
-        table[currItem][currCapacity] = knapsackRecursive(currCapacity, items, currItem - 1, table);
+        table[currItem][currCapacity] = knapsack(currCapacity, items, currItem - 1, table);
         return table[currItem][currCapacity];
     }
 
     // Recursive case: Store the maximum value by either including or excluding the current item
-
     // Including the item:
-    int inclVal = items[currItem].value + knapsackRecursive(currCapacity - items[currItem].weight, items, currItem -1, table);
-    int exclVal = knapsackRecursive(currCapacity, items, currItem - 1, table);
+    int inclVal = items[currItem].value + getValue(items, knapsack(currCapacity - items[currItem].weight, items, currItem -1, table));
+    int exclVal = getValue(items, knapsack(currCapacity, items, currItem - 1, table));
+    // int inclVal = items[currItem].value + knapsack(currCapacity - items[currItem].weight, items, currItem -1, table, pickedItems);
+    //int exclVal = knapsack(currCapacity, items, currItem - 1, table, pickedItems);
 
     if (inclVal > exclVal) {
         // add element to vector in table
-        table[currItem][currCapacity] = inclVal;
-    } else {
-        table[currItem][currCapacity] = exclVal;
+        table[currItem][currCapacity].push_back((int)currItem);
     }
     return table[currItem][currCapacity];
 }
